83bdae2c719cdd0829830ab5378cd3293d04364a
Abhinav yadav <Abhi>
2025-11-07 18:58:00 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,22 @@ import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import "@solana/wallet-adapter-react-ui/styles.css";
+import { Routes, Route } from "react-router-dom";
 import TokenLaunchPad from "./components/TokenLaunchPad";
+import TokenCreation from "./components/TokenCreation";
+import Home from "./components/Home";
+import SupplyToken from "./components/SupplyToken";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <ConnectionProvider endpoint={"https://api.devnet.solana.com/"}>
       <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
           <WalletMultiButton />
+          {/* <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create-token" element={<TokenCreation />} />
+            <Route path="/supply-token" element={<SupplyToken />} />
+          </Routes> */}
           <TokenLaunchPad />
         </WalletModalProvider>
       </WalletProvider>
